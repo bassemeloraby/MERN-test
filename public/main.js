e15f139684320246691bfcbb8538c0fc313706ca
bassem eloraby <BASEM12683@GMAIL.COM>
2022-11-07 01:34:06 +0300

    rect install

---
+++ public/main.js
@@ -0,0 +1,1 @@
+alert('hallooooooooo from javascript')
--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+//set the static assets
+app.use(express.static('public'))
 
 const port = process.env.PORT || '3000';
 
@@ -74,6 +76,11 @@ app.put('/update/:id', (req, res) => {
     else res.redirect('/')
   })
 });
+
+//admin routes
+app.get('/admin',(req,res)=>{
+  res.render('admin')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
