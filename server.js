c2dfa1739e8ab4dfa63198639694d696f0f06b49
bassem eloraby <BASEM12683@GMAIL.COM>
2022-11-07 01:00:02 +0300

    end backend

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const methodOverride = require('method-override');
+const { render } = require('ejs');
+//Lets you use HTTP verbs such as PUT or DELETE in places
+// where the client doesn't support it.
+
 app.use(express.urlencoded({ extended: true }));
+
+app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 const port = process.env.PORT || '3000';
-
-
 
 //mongoodb.connect
 mongoose.connect(
@@ -31,33 +36,43 @@ const schema = new mongoose.Schema({
 const Task = mongoose.model('Task', schema);
 
 //insert
-app.get('/create/:title', (req, res) => {
-  const firstTask = new Task({ title: req.params.title });
-  firstTask.save().then(() => console.log('new task saved'));
+app.post('/create', (req, res) => {
+  const firstTask = new Task({ title: req.body.title });
+  firstTask.save().then(() => res.redirect('/'));
 });
 //show
 app.get('/', (req, res) => {
-  Task.find({},(error,tasks)=>{
+  Task.find({}, (error, tasks) => {
     if (error) console.log(`there is an error ${error}`);
-    else {res.render('todo.ejs')}
-  })
+    else {
+      res.render('todo.ejs', { todotasks: tasks });
+    }
+  });
 });
 //delete
-app.get('/delete/:id', (req, res) => {
- Task.deleteOne({_id: req.params.id},(error)=>{
-  if (error) console.log(`there is an error ${error}`)
-  else {
-    console.log('one task deleted successfully')
-  }
- })
+app.delete('/delete/:id', (req, res) => {
+  Task.deleteOne({ _id: req.params.id }, (error) => {
+    if (error) console.log(`there is an error ${error}`);
+    else {
+      res.redirect('/')
+    }
+  });
 });
+
 //update tasks
-app.get('/update/:id/:title', (req, res) => {
- Task.updateOne({_id: req.params.id},{title: req.params.title},(error)=>{
-  if (error) console.log(`there is an error ${error}`)
-  else {
-    console.log('task is updated successfully!')
-  }})
+app.get('/update/:id', (req, res) => {
+  const id = req.params.id;
+  Task.find({}, (err, tasks) => {
+    res.render('todoEdit.ejs', { todotasks: tasks, idTask: id });
+  });
+});
+//update save 
+app.put('/update/:id', (req, res) => {
+  const id = req.params.id;
+  Task.findByIdAndUpdate(id, {title:req.body.title},err=>{
+    if(err) return res.send(500,err)
+    else res.redirect('/')
+  })
 });
 
 app.listen(port, () => {
