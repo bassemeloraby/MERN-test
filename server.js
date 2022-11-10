c02cad73dc22deb48c2b95712e92f50ae3651466
bassem eloraby <BASEM12683@GMAIL.COM>
2022-11-10 07:44:47 +0300

    axios

--- server.js
+++ server.js
@@ -1,33 +1,30 @@
 const express = require('express');
 
-const cors = require('cors')
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
 const methodOverride = require('method-override');
 const { render } = require('ejs');
 
-require('dotenv').config()
+require('dotenv').config();
 //Lets you use HTTP verbs such as PUT or DELETE in places
 // where the client doesn't support it.
 
-
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 //set the static assets
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 const port = process.env.PORT || '3000';
 
-
 //mongoodb.connect
-const uri = process.env.ATLAS_URI
-
+const uri = process.env.ATLAS_URI;
 
 mongoose.connect(
   uri,
@@ -47,13 +44,20 @@ const schema = new mongoose.Schema({
 
 const Task = mongoose.model('Task', schema);
 
+//routes
+//json route
+app.get('/api/tasks', (req, res) => {
+  Task.find()
+    .then((tasks) => res.json(tasks))
+    .catch((err) => res.status(404).json('Error:' + err));
+});
 //insert
 app.post('/create', (req, res) => {
   const firstTask = new Task({ title: req.body.title });
   firstTask.save().then(() => res.redirect('/'));
 });
 //show
-app.get('/', (req, res) => {
+app.get('/show', (req, res) => {
   Task.find({}, (error, tasks) => {
     if (error) console.log(`there is an error ${error}`);
     else {
@@ -66,7 +70,7 @@ app.delete('/delete/:id', (req, res) => {
   Task.deleteOne({ _id: req.params.id }, (error) => {
     if (error) console.log(`there is an error ${error}`);
     else {
-      res.redirect('/')
+      res.redirect('/');
     }
   });
 });
@@ -78,19 +82,19 @@ app.get('/update/:id', (req, res) => {
     res.render('todoEdit.ejs', { todotasks: tasks, idTask: id });
   });
 });
-//update save 
+//update save
 app.put('/update/:id', (req, res) => {
   const id = req.params.id;
-  Task.findByIdAndUpdate(id, {title:req.body.title},err=>{
-    if(err) return res.send(500,err)
-    else res.redirect('/')
-  })
+  Task.findByIdAndUpdate(id, { title: req.body.title }, (err) => {
+    if (err) return res.send(500, err);
+    else res.redirect('/');
+  });
 });
 
 //admin routes
-app.get('/admin',(req,res)=>{
-  res.render('admin')
-})
+app.get('/admin', (req, res) => {
+  res.render('admin');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
